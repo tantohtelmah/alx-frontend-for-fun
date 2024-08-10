5eacc35316b839ec4b68a95c2caec2d67f0ab9e8
tantohtelmah <tantohtelmah@gmail.com>
2024-08-10 19:36:03 -0400

    tasks

--- markdown2html.py
+++ markdown2html.py
@@ -31,10 +31,6 @@ def convert_markdown_to_html(markdown_file, output_file):
     sys.exit(0)
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: ./markdown2html.py <input_file.md> <output_file.html>", file=sys.stderr)
-        sys.exit(1)
-
     input_file = sys.argv[1]
     output_file = sys.argv[2]
 
